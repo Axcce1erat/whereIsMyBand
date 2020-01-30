26160c6659b97452878fcc0ebd4184e4fea4b9e7
Malte Knezevic <m.knezevic@telekom.de>
2020-01-30 09:44:33 +0100

    Bandcontroller geändert

--- ourProject/src/main/java/com/whereIsMyBand/ourProject/controller/BandController.java
+++ ourProject/src/main/java/com/whereIsMyBand/ourProject/controller/BandController.java
@@ -26,8 +26,8 @@ import java.util.List;
 @Controller
 public class BandController {
 
-        @Autowired
-        private BandRepository bandRepository;
+	@Autowired
+	private BandRepository bandRepository;
 
 	@Autowired
 	private SkillRepository skillRepository;
@@ -35,48 +35,114 @@ public class BandController {
 	@Autowired
 	private StyleRepository styleRepository;
 
-	 @Autowired
-        private RoleRepository roleRepository;
+	@Autowired
+	private RoleRepository roleRepository;
 
-    @GetMapping("/bands")
-    public String getAll(Model model) {
+	@GetMapping("/bands")
+	public String getAll(Model model) {
 
-            model.addAttribute("bands", bandRepository.findAll());
+		model.addAttribute("bands", bandRepository.findAll());
 
-        return "bands";
-    }
+		return "bands";
+	}
 
-    @GetMapping("/band")
-    public String getBand(Model model,
-                            @RequestParam(required = false) Long id) {
+	@GetMapping("/band")
+	public String getBand(Model model,
+			@RequestParam(required = false) Long id) {
 
-                            Band band = new Band();
-        if (id != null) {
-            Optional<Band> optionalBand = bandRepository.findById(id);
-            if (optionalBand.isPresent()) {
-                band = optionalBand.get();
-            }
-        }
-        model.addAttribute("band", band);
+			Band band = new Band();
+			if (id != null) {
+				Optional<Band> optionalBand = bandRepository.findById(id);
+				if (optionalBand.isPresent()) {
+					band = optionalBand.get();
+				}
+			}
+
+			//Hier beginnt der Versuch des calls der Roles
+			model.addAttribute("band", band);
+			model.addAttribute("allRoles", roleRepository.findAll());
+			model.addAttribute("allStyles", styleRepository.findAll());
+			model.addAttribute("allSkills", skillRepository.findAll());
+
+			// call the method getRoles in Band
+
+			List<Role> roles = new ArrayList<>();
+			Method methodRole = getMethod(band, "getRoles",
+					new Class[]{});
+			if (methodRole != null) {
+				try {
+					roles = (List<Role>) methodRole.invoke(band);
+				} catch (IllegalAccessException | InvocationTargetException e) {
+					e.printStackTrace();
+				}
+			}
+
+			model.addAttribute("bandRoles", roles);
 
 
-        return "band";
-    }
+			//call the method getStyles in Band
 
-    @PostMapping("/band")
-    public String postSchool(@ModelAttribute Band band) {
+			List<Style> styles = new ArrayList<>();
+			Method methodStyle = getMethod(band, "getStyles",
+					new Class[]{});
+			if (methodStyle != null) {
+				try {
+					styles = (List<Style>) methodStyle.invoke(band);
+				} catch (IllegalAccessException | InvocationTargetException e) {
+					e.printStackTrace();
+				}
+			}
 
-            bandRepository.save(band);
+			model.addAttribute("bandStyles", roles);
 
-        return "redirect:/bands";
-    }
 
-    @GetMapping("/band/delete")
-    public String deleteBand(@RequestParam Long id) {
+			//call the method getSkills in Band
 
-            bandRepository.deleteById(id);
+			List<Skill> skills = new ArrayList<>();
+			Method methodSkill = getMethod(band, "getSkills",
+					new Class[]{});
+			if (methodSkill != null) {
+				try {
+					skills = (List<Skill>) methodSkill.invoke(band);
+				} catch (IllegalAccessException | InvocationTargetException e) {
+					e.printStackTrace();
+				}
+			}
 
-        return "redirect:/bands";
-    }
+			model.addAttribute("bandSkills", skills);
+
+
+			return "band";
+}
+
+@PostMapping("/band")
+public String postSchool(@ModelAttribute Band band) {
+
+	bandRepository.save(band);
+
+	return "redirect:/bands";
+}
+
+@GetMapping("/band/delete")
+public String deleteBand(@RequestParam Long id) {
+
+	bandRepository.deleteById(id);
+
+	return "redirect:/bands";
+}
+
+//Hier ist die getMethod
+
+public Method getMethod(Object obj, String methodName, Class[] args) {
+	Method method;
+	try {
+		method = obj.getClass().getDeclaredMethod(methodName, args);
+		return method;
+	} catch (NoSuchMethodException e) {
+		e.printStackTrace();
+	}
+	return null;
+}
+
 }
 
