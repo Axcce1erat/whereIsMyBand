afc09b53769eb282d5f547620834d941fec62a2f
Berthold Voitl <berthold.voitl@telekom.de>
2020-02-03 12:03:20 +0100

    Login für Mail-Bereich und Danke-Seite

---
+++ ourProject/src/main/java/com/whereIsMyBand/ourProject/config/MySecurityConfig.java
@@ -0,0 +1,53 @@
+package com.whereIsMyBand.ourProject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+	@Configuration
+	@EnableWebSecurity
+	public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+	    
+		@Override
+	    protected void configure(HttpSecurity http) throws Exception {
+			http     					
+			.authorizeRequests()
+		    	.antMatchers("/mailoutput").hasRole("ADMIN")
+		  //  	.and()
+	      //  .formLogin()
+	            .and()
+	        .httpBasic();
+			}   	
+	  
+		
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+			
+		auth.inMemoryAuthentication()
+	    .withUser("Malte")    	
+	    	.password(encoder.encode("music"))
+	    	.roles("ADMIN")
+	        .and()
+	    .withUser("Denis")
+	    	.password(encoder.encode("music"))
+			.roles("ADMIN")
+			.and()
+	    .withUser("Axel")
+	        .password(encoder.encode("music"))
+	        .roles("ADMIN")
+	        .and()
+		.withUser("Bert")
+        	.password(encoder.encode("music"))
+        	.roles("ADMIN");
+		}
+	}
+
+
+	
+
+
