5766ba79c200925414b23a15d71c17bf0ae6f2ac
Berthold Voitl <berthold.voitl@telekom.de>
2020-02-05 11:36:40 +0100

    Templates implementiert, Band anlegen nur für ADMIN

--- ourProject/src/main/java/com/whereIsMyBand/ourProject/config/MySecurityConfig.java
+++ ourProject/src/main/java/com/whereIsMyBand/ourProject/config/MySecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 			http     					
 			.authorizeRequests()
 		    	.antMatchers("/mailoutput").hasRole("ADMIN")
+		    	.antMatchers("/band").hasRole("ADMIN")
 		  //  	.and()
 	      //  .formLogin()
 	            .and()
