dbb3b625aec90c05f220c434ac69dfd418eff890
Berthold Voitl <berthold.voitl@telekom.de>
2020-02-11 14:28:28 +0100

    Log-In admin generiert beim Hochfahren

---
+++ ourProject/src/main/java/com/whereIsMyBand/ourProject/bootstrap/BootstrapAdminUser.java
@@ -0,0 +1,40 @@
+package com.whereIsMyBand.ourProject.bootstrap;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.whereIsMyBand.ourProject.entity.user.User;
+import com.whereIsMyBand.ourProject.repository.user.UserRepository;
+
+@Component
+public class BootstrapAdminUser implements CommandLineRunner {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public BootstrapAdminUser (UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		super();
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+	 Optional<User> optionalAdmin = userRepository.findByName("admin");
+	 if (optionalAdmin.isEmpty()) {
+		 System.out.println("No admin user, creating admin");
+		 User admin = new User();
+		 admin.setName("admin");
+		 admin.setRole("ADMIN");
+		 admin.setPassword(passwordEncoder.encode("music"));
+		 userRepository.save(admin);
+	  }
+	}
+	
+}
--- ourProject/src/main/java/com/whereIsMyBand/ourProject/config/MySecurityConfig.java
+++ ourProject/src/main/java/com/whereIsMyBand/ourProject/config/MySecurityConfig.java
@@ -1,10 +1,12 @@
 package com.whereIsMyBand.ourProject.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,12 +17,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 	@EnableWebSecurity
 	public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 	    
+		private final UserDetailsService userDetailsService;
+
+		private final PasswordEncoder passwordEncoder;
+		
+		@Autowired
+		public MySecurityConfig (UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+			super();
+			this.userDetailsService = userDetailsService;
+			this.passwordEncoder = passwordEncoder;
+		}
+		
+
 		@Override
 	    protected void configure(HttpSecurity http) throws Exception {
 			http     					
 			.authorizeRequests()
-		    	.antMatchers("/mailoutput").hasRole("ADMIN")
-		    	.antMatchers("/band").hasRole("ADMIN")
+		    	.antMatchers("/", "/bands", "/mails", "/pictures/**", "/css/**", "/fragments/**").permitAll()
+				.anyRequest().authenticated()
 		    	.and()
 		    .formLogin()
 	            .and()
@@ -30,98 +44,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 		
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-			
-		auth.inMemoryAuthentication()
-	    .withUser("Malte")    	
-	    	.password(encoder.encode("music"))
-	    	.roles("ADMIN")
-	        .and()
-	    .withUser("Denis")
-	    	.password(encoder.encode("music"))
-			.roles("ADMIN")
-			.and()
-	    .withUser("Axel")
-	        .password(encoder.encode("music"))
-	        .roles("ADMIN")
-	        .and()
-		.withUser("Bert")
-        	.password(encoder.encode("music"))
-        	.roles("ADMIN");
-		}
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
 	}
-
-
-//
-//	@Configuration
-//	@EnableWebSecurity
-//	public class MySecurityConfig extends WebSecurityConfigurerAdapter {
-//		    
-//		
-//		@Qualifier("userDetailsServiceImpl")
-//		@Autowired
-//		private UserDetailsService userDetailsService;
-//
-//		    @Bean
-//		    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//		        return new BCryptPasswordEncoder();
-//		    }
-//		
-//		
-//		@Override
-//	    protected void configure(HttpSecurity http) throws Exception {
-//			http     					
-//			.authorizeRequests()
-//		    	.antMatchers("/mails", "/mailoutput", "/band", "/bands", "/", "registration").permitAll()
-//		    	.anyRequest().authenticated()
-//				.and()
-//		   	.formLogin()
-//		   		.loginPage("/login")
-//		   		.permitAll()
-//	            .and()
-//	        .logout()
-//	        .permitAll();
-//			}   	
-//	  
-//		
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//			
-//		auth.inMemoryAuthentication()
-//	    .withUser("Malte")    	
-//	    	.password(encoder.encode("music"))
-//	    	.roles("ADMIN")
-//	        .and()
-//	    .withUser("Denis")
-//	    	.password(encoder.encode("music"))
-//			.roles("ADMIN")
-//			.and()
-//	    .withUser("Axel")
-//	        .password(encoder.encode("music"))
-//	        .roles("ADMIN")
-//	        .and()
-//		.withUser("Bert")
-//        	.password(encoder.encode("music"))
-//        	.roles("ADMIN");
-//		}
-//	
-//	@Bean
-//    public AuthenticationManager customAuthenticationManager() throws Exception {
-//        return authenticationManager();
-//    }
-//
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-//    }
-//}
-//	
-	
-	
-	
-
-
-	
-
+}
 
